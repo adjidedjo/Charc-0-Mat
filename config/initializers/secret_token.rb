7f94c5b93689c217a7a1464d207569b3789c5ddc
Aji Yulianto <aji.yulianto@kiranatama.com>
2012-03-18 15:04:18 +0700

    creating new repository application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Charcomat::Application.config.secret_token = '4218beaf5365c9e4dd9aa77786516b427932813060831c24c637b092d083a4d4be1d490cf61fd2e051f659ac3c328936e42691150839f40ff6aa6157f673e350'
